6fa0015ce2b9dc0e368d1cdd219fdd4bcab46966
Mohammed Idris <160055397+MoIdris@users.noreply.github.com>
2024-07-27 14:16:20 +0000

    Update relevant files

--- frontend/main.py
+++ frontend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a58fce4c4e8ba4663dc05f79173da4541749159b8e820046ad98be26c6056641
-size 7744
+oid sha256:a2c92a33d97bf7071b394ddaa59d4adbb82ecdb3f8d3cd34db85dc1d5e2d89c1
+size 7673
